44f2852d1e21163051565ec5ff3c5382c17b1775
chatt <arya_c.cse2021@msit.edu.in>
2023-08-25 21:04:12 +0530

    commit1

---
+++ java_development/src/variables/Input.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Input {
+
+	public Input() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
